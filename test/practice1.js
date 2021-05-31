2a18735f522d639aea7b18e3b79a6efc47430c7f
kaito101006 <85086944+kaito101006@users.noreply.github.com>
2021-05-31 12:27:07 +0900

    Delete test directory

--- test/practice1.js
+++
@@ -1,9 +0,0 @@
-const typing = (element, sentence) => {
-     [...sentence].forEach((character, index) => {
-       setTimeout(() => {
-         document.querySelector(element).textContent += character;
-       }, 100 * ++index);
-     });
-   }
-   
-   typing('#typing', 'Welcome My Home Page!!');
